776fda21c001d9859ba227a2a1d346eb021366f1
Ricardo Watson <Rickangelodan31@gmail.com>
2024-05-10 13:57:20 +0200

    finish with responsiveness

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,35 @@
+import React, { useState } from "react";
+import classes from "./post.module.css";
+
 const Post = ({ post }) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    setShowOverlay(true);
+  };
+
+  const handleCloseOverlay = () => {
+    setSelectedImage(null);
+    setShowOverlay(false);
+  };
+
   return (
-    <div>
+    <div className={classes.post}>
       <h2>{post.title}</h2>
       <p>Vendor: {post.vendor.username}</p>
       <p>Description: {post.description}</p>
-      <img src={post.image} alt={`title ${post.title}`} />
+      <img
+        src={post.image}
+        alt={`Title: ${post.title}`}
+        onClick={() => handleImageClick(post.image)}
+      />
+      {showOverlay && (
+        <div className={classes.overlay} onClick={handleCloseOverlay}>
+          <img src={selectedImage} alt={`Title: ${post.title}`} />
+        </div>
+      )}
     </div>
   );
 };
